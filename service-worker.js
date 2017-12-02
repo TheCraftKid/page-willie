ac5441a5f3aedfbb35c9fa559668f5ecf4939196
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-02 00:42:49 -0600

    Add caching to service worker

--- service-worker.js
+++ service-worker.js
@@ -16,3 +16,13 @@ self.addEventListener('push', (event) => {
     })
   );
 });
+
+
+self.addEventListener('fetch', (event) => {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      }));
+});
