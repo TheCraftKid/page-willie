cfa749a4745bb17665880a1fce59ecbc47dc2f2b
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-04 07:46:27 -0600

    Create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+
+const gulp = require('gulp');
+const mergeStream = require('merge-stream');
+const minify = require('uglify-js');
+
+const client = require('firebase-tools');
+const builder = require('polymer-build');
+const PolymerProject = require('polymer-build').PolymerProject;
+
+const PROJECT_PATH = '.';
+const FIRBASE_PROJECT = require('./.firebaserc').project.default;
+
+const project = new PolymerProject(require('./polymer.json'));
+
+gulp.task('default', () => {
+  console.log('Hello, world!');
+});
+
+gulp.task('build', () => {
+  return gulp.src(['src/**/*.html'])
+    .pipe(minify())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build:functions', () => {
+  gulp.src(['functions']);
+});
+
+gulp.task('watch', () => {
+  const watcher = gulp.watch('*.js', ['build']);
+  watcher.on('change', (event) => {
+    console.log(`File at ${event.path} was modified, rebuilding...`);
+  });
+});
+
+gulp.task('deploy', ['deploy:functions', 'deploy:site']);
+
+gulp.task('deploy:functions', () => {
+
+});
+
+gulp.task('deploy:site', () => {
+  client.deploy({
+    project: FIRBASE_PROJECT,
+    token: process.event.FIREBASE_TOKEN,
+    cwd: PROJECT_PATH,
+  }).then(() => {
+
+  }).catch((err) => {
+    console.error('Error when deploying site:', err);
+  });
+});
+
+gulp.task('generatePushManifest', () => {
+  mergeStream(project.sources(), project.dependencies())
+    .pipe(project.addPushManifest())
+    .pipe(gulp.dest('build/'));
+});
+
+function injectFirebase() {
+  // return function
+}
