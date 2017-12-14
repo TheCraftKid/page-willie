3b2dd615d6fe56cd42ded41fb3ba9bf6a7f8dd9e
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-13 23:00:05 -0600

    Add notification handling to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,65 @@
+const NOTIFICATION_ICON = '/images/icon-192x192.png';
+const KEY_REQUEST_FULFILLED = 'page-request-fulfilled';
+const KEY_REQUEST_REJECTED = 'page-request-rejected';
+
 const TITLE_READY = 'Willie is now ready for you.';
-const TITLE_BUMP = 'You have been removed from the wait list.';
+const TITLE_REJECTED = 'You have been removed from the wait list.';
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }));
+});
 
 self.addEventListener('push', (event) => {
   console.log('Received a push message', event);
 
-  let body = 'We have received a push message.';
-  let icon = '/images/icon-192x192.png';
-  let tag = 'page-willie-notification-tag';
+  if (!(self.notification && self.notification.permission === 'granted')) {
+    // We don't have access to notify user.
+    return;
+  }
+
+  const data = {};
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  let notification;
+  if (data[KEY_REQUEST_FULFILLED]) {
+    notification = new Notification(TITLE_READY, {
+      body: TITLE_READY,
+      icon: NOTIFICATION_ICON,
+      tag: KEY_REQUEST_FULFILLED,
+    });
+  } else if (data[KEY_REQUEST_REJECTED]) {
+    notification = new Notification(TITLE_REJECTED, {
+      body: TITLE_REJECTED,
+      icon: NOTIFICATION_ICON,
+      tag: KEY_REQUEST_REJECTED,
+    });
+  }
+
+  notification.addEventListener('click', () => {
+    if (clients.openWindow) {
+      clients.openWindow('//pagewillie.com/onNotify');
+    }
+  });
 
   event.waitUntil(
-    self.registration.showNotification(TITLE_READY, {
-      body: body,
-      icon: icon,
-      tag: tag,
-    })
+    self.registration.showNotification(notification)
   );
 });
 
+self.addEventListener('notificationClick', (event) => {
+  event.notification.close();
+  let responsePromise = Promise.resolve();
+  if (event.notification.data && event.notification.data.url) {
+    responsePromise = clients.openWindow(event.notification.data.url);
+  }
 
-self.addEventListener('fetch', (event) => {
-  console.log(event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      }));
+  event.waitUntil(Promise.all([
+    responsePromise,
+    // TODO: Track click
+  ]));
 });
