71f54f48430fe3552c621d6515b1bec04c722ac5
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-16 16:54:50 -0600

    Move auth.js into global scope

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,27 +1,37 @@
 'use strict';
-
-let lastUser = null;
-
 (function init() {
+  let lastUser = null;
+
   firebase.auth().onAuthStateChanged((user) => {
     lastUser = user;
   });
+
+  /**
+   * Returns the currently signed in user's name if one exists.
+   *
+   * @return {String} The name of the signed in user
+   */
+  function getDisplayName() {
+    return lastUser ? lastUser.displayName : '';
+  }
+
+  /**
+   * Returns the currently signed in user's name if one exists.
+   *
+   * @return {String} The profile URL of the currently signed in user.
+   */
+  function getImageUrl() {
+    return lastUser ? lastUser.photoURL : '';
+  }
+
+  /**
+   * Returns true if the currently signed in user has admin privileges.
+   * @return {boolean}
+   */
+  function getIsAdmin() {
+    return firebase.database().ref(`/admin/${lastUser.uid}`).once('value')
+      .then((snap) => {
+        return snap.exists();
+      });
+  }
 })();
-
-/**
- * Returns the currently signed in user's name if one exists.
- *
- * @return {String} The name of the signed in user
- */
-function getDisplayName() {
-  return lastUser ? lastUser.displayName : '';
-}
-
-/**
- * Returns the currently signed in user's name if one exists.
- *
- * @return {String} The profile URL of the currently signed in user.
- */
-function getImageUrl() {
-  return lastUser ? lastUser.photoURL : '';
-}
