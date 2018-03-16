57f015de4254732c77a4c85c34e322632008a752
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-16 15:24:59 -0500

    Scaffold index.ts  - Set up request fulfillment  - Set up user data management  - Set up database triggers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as flamelink from 'flamelink';
+import { handleHttpRequest, handleNewRequest, handleUpdatedRequest } from './requests';
+import { fetchAdminList } from './admin';
+import { createUserRecord, deleteUserRecord } from './users';
+import { fetchBlogPosts } from './posts';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const firebaseApp = admin.initializeApp(functions.config().firebase)
+const flamelinkApp = flamelink({
+  firebaseApp,
+  isAdminApp: true,
+});
+
+export const fulfillRequest = functions.https.onRequest(handleHttpRequest);
+
+export const getAdmins = functions.https.onRequest(fetchAdminList);
+
+export const getBlogPosts = functions.https.onRequest(fetchBlogPosts);
+
+export const onUserCreated = functions.auth.user().onCreate(createUserRecord);
+
+export const onUserDeleted = functions.auth.user().onCreate(deleteUserRecord);
+
+export const onDbRequest = functions.firestore.document('requests/{requestId}')
+  .onCreate(handleNewRequest);
+
+export const onRequestUpdate = functions.firestore.document('requests/{requestId}')
+  .onUpdate(handleUpdatedRequest);
